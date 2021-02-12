f47d95cff6321fa846161020ccc3be01ed9114c9
guipernicone <guipernicone@hotmail.com>
2021-02-12 15:45:38 -0300

    Add the spring project

---
+++ src/main/java/com/zup/cqc/CasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package com.zup.cqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDoCodigoApplication.class, args);
+	}
+
+}
